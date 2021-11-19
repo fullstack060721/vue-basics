588cbc04754e63591b144cd497aa7c5dd1cc680a
micserv010821 <dotnet290320@gmail.com>
2021-11-19 09:29:45 +0200

    upload code

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const app = Vue.createApp({
+  template: `<h2>I am the template</h2>
+        <p>{{ title }} - by {{ author }}, {{ age }} years old</p>`,
     data() {
       return {
         title: 'The Way of Kings',
